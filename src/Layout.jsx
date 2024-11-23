82e658616063cbfbac6b26632fe1d25962829974
bongtta <kce0419@naver.com>
2024-11-24 03:12:33 +0900

    feat: door 페이지 구현

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
 import Header from './components/common/Header';
+import Door from './pages/groups/Door';
 const Layout = () => {
   return (
     <>
       <Header />
       <Outlet />
+      <Door />
     </>
   );
 };
--- src/pages/groups/Door.jsx
+++ src/pages/groups/Door.jsx
@@ -1,7 +1,58 @@
 import React from 'react';
+import styled from 'styled-components';
 
 const Door = () => {
-  return <div>생성 / 참여 선택 버튼</div>;
+  return (
+    <Wrapper>
+      <Up>
+        <UpTitle>
+          너도 <span>번따</span> 할래?
+        </UpTitle>
+        <UpDetail>
+          우리 모임의 사람들 중<br />
+          랜덤으로 매칭 받고 친해지자!
+        </UpDetail>
+      </Up>
+
+      <div>
+        <button type="submit">
+          방 만들래 <span>새로운 모임을 만들어요</span>
+        </button>
+        <button type="submit">
+          참여할래 <span>기존 모임에 참가해요</span>
+        </button>
+      </div>
+
+      <p>
+        네가 관심 있던 <span>그 사람</span>과 매칭 될지도 몰라!
+      </p>
+    </Wrapper>
+  );
 };
 
 export default Door;
+
+const Wrapper = styled.div`
+  padding: 0 2.4rem;
+`;
+
+const Up = styled.div`
+  display: flex;
+  /* width: 196px; */
+  flex-direction: column;
+  /* align-items: flex-start; */
+  margin-top: 9rem;
+  gap: 1.5rem;
+`;
+
+const UpTitle = styled.p`
+  ${({ theme }) => theme.font.heading1};
+
+  & span {
+    color: ${({ theme }) => theme.color.primary};
+  }
+`;
+
+const UpDetail = styled.p`
+  ${({ theme }) => theme.font.heading5};
+`;
