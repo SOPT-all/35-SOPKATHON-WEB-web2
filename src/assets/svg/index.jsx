e2369ed24d8a167eb7a7fa530cc44c58ee4503aa
Parkchaeyeon <ccyy1029@naver.com>
2024-11-24 04:15:44 +0900

    feat: Info 페이지 퍼블리싱

--- src/assets/svg/index.jsx
+++ src/assets/svg/index.jsx
@@ -1,2 +1,3 @@
 export { default as BtnLetterCloseHintNoText } from './BtnLetterCloseHintNoText';
+export { default as BtnLetterInformation } from './BtnLetterInformation';
 export { default as BtnLetterOpenHintNoText } from './BtnLetterOpenHintNoText';
