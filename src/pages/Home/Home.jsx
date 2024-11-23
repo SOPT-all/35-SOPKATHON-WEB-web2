bdbb0f56d2d1f0261b10496a2035bc32c8a89930
bongtta <kce0419@naver.com>
2024-11-24 02:09:21 +0900

    feat: 로그인 페이지 구현

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,11 +2,102 @@ import React from 'react';
 import styled from 'styled-components';
 
 const Home = () => {
-  return <HomeLayout>우리팀 화이팅</HomeLayout>;
+  return (
+    <Wrapper>
+      <Logo src="public/logo.png" />
+
+      <Main>
+        <PhoneNumber>
+          <Text>전화번호</Text>
+          <InputNumber type="text" name="phoneNumber" placeholder="010-0000-0000" />
+        </PhoneNumber>
+
+        <Password>
+          <Text>비밀번호</Text>
+          <InputPassword type="text" name="password" placeholder="비밀번호 6자리를 입력해주세요" />
+        </Password>
+      </Main>
+
+      <SubmitButton type="submit">번호 따러 갈래?</SubmitButton>
+    </Wrapper>
+  );
 };
 
 export default Home;
 
-export const HomeLayout = styled.div`
-  ${({ theme }) => theme.font.heading1_bold_20}
+export const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 0 2.4rem;
+`;
+
+export const Logo = styled.img`
+  height: 15.9rem;
+  margin-top: 9.5rem;
+`;
+
+export const Main = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  gap: 4rem;
+`;
+
+export const PhoneNumber = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin-top: 3.8rem;
+  gap: 0.8rem;
+`;
+
+export const Text = styled.p`
+  color: ${({ theme }) => theme.color.gray8};
+  ${({ theme }) => theme.font.body3};
+`;
+
+export const InputNumber = styled.input`
+  height: 5rem;
+  padding: 1.5rem 1.3rem;
+  align-items: center;
+  border-radius: 10px;
+  border: 1px solid ${({ theme }) => theme.color.gray6};
+  ${({ theme }) => theme.font.body2};
+
+  &::placeholder {
+    ${({ theme }) => theme.font.body2};
+    color: ${({ theme }) => theme.color.gray4};
+  }
+`;
+
+export const Password = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 0.8rem;
+`;
+
+export const InputPassword = styled.input`
+  height: 5rem;
+  padding: 1.5rem 1.3rem;
+  align-items: center;
+  border-radius: 10px;
+  border: 1px solid ${({ theme }) => theme.color.gray6};
+  ${({ theme }) => theme.font.body2};
+
+  &::placeholder {
+    ${({ theme }) => theme.font.body2};
+    color: ${({ theme }) => theme.color.gray4};
+  }
+`;
+
+export const SubmitButton = styled.button`
+  /* position: absolute; */
+  width: 100%;
+  height: 5.6rem;
+  margin-top: 6.3rem;
+  /* margin-bottom: 4.4rem; */
+  border-radius: 10px;
+  background-color: ${({ theme }) => theme.color.primary};
+  color: ${({ theme }) => theme.color.white};
+  ${({ theme }) => theme.font.body1};
 `;
